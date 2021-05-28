ffc16f949d1513ac40c31d8a8211d66df4a76461
NabinKhadka1 <nabinkhadka330@gmail.com>
2021-05-28 14:31:13 +0545

    Added an app

--- myproject/apps/magazine/apps.py
+++ myproject/apps/magazine/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class MagazineConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'magazine'
+class MagazineAppConfig(AppConfig):
+    name = 'myproject.apps.magazine'
